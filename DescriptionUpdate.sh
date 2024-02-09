b502580b687ed0d8f00de119f5fc39a3212dfd76
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-02-09 12:51:17 -0500

    🚀 Add shell script  (#2)
    * add test branch
    
    * update test branch
    
    * update test branch
    
    * remove /n
    
    * add new line
    
    * remove old action;merge shell
    
    * fix token part
    
    * try new fq
    
    * normalize
    
    * remove debugs
    
    * add fe relates files
    
    * change description
    
    * fix condition
    
    * fix if
    
    * last if
    
    * fix token name
    
    * add new lines
    
    * add new lines

---
+++ DescriptionUpdate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "Error: GITHUB_TOKEN input is required"
+  exit 1
+fi
+
+if [ -z "$BODY" ]; then
+  echo "Error: body input is required"
+  exit 1
+fi
+
+# Get pull request number
+PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+if [ "$PR_NUMBER" == "null" ]; then
+  echo "Error: No pull request found"
+  exit 1
+fi
+
+# Fetch current pull request details
+CURRENT_BODY=$(gh pr view $PR_NUMBER --json body -q .body)
+
+# Normalize the formatting of the new body for comparison
+NORMALIZED_BODY=$(echo "$BODY" | sed 's/ *//g' | tr -d '\n')
+
+# Check if newBody already exists in the current description
+if ! echo "$CURRENT_BODY" | sed 's/ *//g' | tr -d '\n' | grep -Fq "$NORMALIZED_BODY"; then
+  # Concatenate the new text to the existing description
+  COMBINED_BODY="${CURRENT_BODY} ${BODY}"
+
+  gh pr edit $PR_NUMBER --body "${COMBINED_BODY}"
+
+  echo "Updated pull request description."
+else
+  echo "New body already exists in the current description. No update needed."
+fi
